b7b7477cb505e72cfe0b0c9a9cbc3e1dbdba8402
imran-pst <imran@cdda.io>
2023-10-12 17:23:55 +0600

    github workflow updated

--- post/tests.py
+++ post/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+class PostTest(TestCase):
+    def setUp(self):
+        Post.objects.create(title="test")
+
+    def test_title_length(self):
+        post = Post.objects.filter(title="test1").first()
+        self.assertEqual(post, None)
